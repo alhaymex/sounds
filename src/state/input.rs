cd972472abed46357197492dd719df3d48b54632
Alharith Yassin <76082322+alhaymex@users.noreply.github.com>
2026-06-10 15:43:08 +0300

    Feat/workflows (#4)
    * add ci.yml
    
    * add release.yml
    
    * adding checksums

---
+++ src/state/input.rs
@@ -0,0 +1,28 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum InputTarget {
+    LibraryPath,
+    Search,
+    // YoutubeSearch,
+}
+
+pub struct TextInputState {
+    pub target: InputTarget,
+    pub value: String,
+}
+
+impl TextInputState {
+    pub fn new(target: InputTarget, value: impl Into<String>) -> Self {
+        Self {
+            target,
+            value: value.into(),
+        }
+    }
+
+    pub fn push_char(&mut self, ch: char) {
+        self.value.push(ch);
+    }
+
+    pub fn pop_char(&mut self) {
+        self.value.pop();
+    }
+}
